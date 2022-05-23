d6b4f3b48f0197eac664c0f8545e634c0e3256fc
Micha Davis <m.leigh.davis@gmail.com>
2022-05-23 13:40:25 -0700

    create Graph Display component

---
+++ src/components/GraphDisplay/index.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+// This will display graphs for the explore and user profile pages
+export default function GraphDisplay() {
+  return <Box sx={{
+    backgroundColor: "darkslategray",
+    minHeight: "200px",
+    minWidth: "200px",
+  }}><Typography>[ ] - [ ] - [ ]</Typography></Box>
+
+}
