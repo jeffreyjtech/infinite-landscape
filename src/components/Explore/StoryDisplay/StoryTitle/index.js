7ca4be8a1cdef9864b4876c54d54bff2c19250aa
Micha Davis <m.leigh.davis@gmail.com>
2022-05-23 13:39:41 -0700

    create StoryTitle component

---
+++ src/components/Explore/StoryDisplay/StoryTitle/index.js
@@ -0,0 +1,5 @@
+import Typography from "@mui/material/Typography";
+
+export default function StoryTitle(props) {
+  return <Typography variant="h4" sx={{ marginBottom: "2%" }}>{props.title}</Typography>
+}
