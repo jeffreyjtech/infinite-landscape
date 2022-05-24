669e0e7c4011f26a815eae27272e439b81537c00
Micha Davis <m.leigh.davis@gmail.com>
2022-05-24 10:36:29 -0700

    add react-graph-vis configure graph vis component

--- src/components/GraphDisplay/index.js
+++ src/components/GraphDisplay/index.js
@@ -1,10 +1,52 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import Graph from "react-graph-vis";
 
 // This will display graphs for the explore and user profile pages
-export default function GraphDisplay() {
-  return <Box sx={{
-    backgroundColor: "darkslategray",
-    minHeight: "160px",
-  }}><Typography>[ ] - [ ] - [ ]</Typography></Box>
+export default function GraphDisplay(props) {
+  const handleClick = ({ items }) => {
+    props.handleClick(items);
+  }
+
+  const handleHover = (id) => {
+    props.handleHover(id);
+  }
+
+  return (
+    <Box sx={{
+      minHeight: "160px",
+    }}>
+      <Graph
+        graph={props.graph}
+        options={{
+          physics: {
+            enabled: false
+          },
+          nodes: {
+            shape: "square",
+            font: {
+              color: "white"
+            }
+          },
+          interaction: {
+            dragNodes: false,
+            dragView: false,
+            selectable: false,
+            zoomView: false
+          },
+          edges: {
+            arrows: {
+              to: false,
+              from: false
+            },
+            color: "white"
+          }
+        }}
+        events={{
+          click: handleClick,
+          hoverNode: handleHover,
+        }}
+      />
+    </Box>
+  );
 
 }
