d0d5587aad587dea14aee51c31e5f51fb1ed8894
Micha Davis <m.leigh.davis@gmail.com>
2022-05-24 10:44:44 -0700

    add more test data

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -5,12 +5,12 @@ import NetworkDisplay from "./NetworkDisplay";
 const stories = {
   nodes: [
     {
-      title: "The first room",
+      label: "The first room",
       username: "micha",
       penName: "Michanations",
-      uuid: 10001,
-      summary: "A simple room with information placqards on one wall.",
-      description: "This room is completely unadorned, save for a gigantic information diagram on the east wall. There is a single door leading out of the room.",
+      id: 10001,
+      summary: "A simple room with information placards on one wall.",
+      description: "This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.",
       category: "TUTORIAL",
       color: "blue",
       tooltips: {
@@ -19,10 +19,10 @@ const stories = {
       }
     },
     {
-      title: "The second room",
+      label: "The second room",
       username: "micha",
       penName: "Michanations",
-      uuid: 10002,
+      id: 10002,
       summary: "An elaborately decorated room.",
       description: "This room is festooned with gaudy decorations. Nearly every surface is covered with some kind of intricate pattern.",
       category: "TUTORIAL",
@@ -31,17 +31,61 @@ const stories = {
         decorations: "Sometimes there will be hidden information in these descriptions",
         pattern: "One repeated motif is a series of colors arranged in a circle, each color signifying a different region of the landscape."
       }
-    }
+    },
+    {
+      label: "The third room",
+      username: "micha",
+      penName: "Michanations",
+      id: 10003,
+      summary: "A simple room with information placards on one wall.",
+      description: "This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.",
+      category: "TUTORIAL",
+      color: "green",
+      tooltips: {
+        information: "You can hover over words like this to get more information",
+        door: "This door leads to the next room. Hover over that room in the map view to see what might be inside."
+      }
+    },
+    {
+      label: "The other third room",
+      username: "micha",
+      penName: "Michanations",
+      id: 10004,
+      summary: "A simple room with information placards on one wall.",
+      description: "This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.",
+      category: "TUTORIAL",
+      color: "purple",
+      tooltips: {
+        information: "You can hover over words like this to get more information",
+        door: "This door leads to the next room. Hover over that room in the map view to see what might be inside."
+      }
+    },
   ],
   edges: [
     {
-      source: 10001,
-      target: 10002
+      from: 10001,
+      to: 10002
     },
     {
-      source: 10002,
-      target: 10001
-    }
+      from: 10002,
+      to: 10001
+    },
+    {
+      from: 10002,
+      to: 10003
+    },
+    {
+      from: 10002,
+      to: 10004
+    },
+    {
+      from: 10004,
+      to: 10002
+    },
+    {
+      from: 10003,
+      to: 10002
+    },
   ]
 };
 let currentStory = stories.nodes[0];
