d45b29487a82d0bf3ac4cdfc47c48946a3d0325c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-25 10:43:40 -0700

    Added a blank option to category select

--- src/components/Explore/NetworkDisplay/NetworkControls/index.js
+++ src/components/Explore/NetworkDisplay/NetworkControls/index.js
@@ -1,26 +1,29 @@
-import { Box, Button, MenuItem, Select } from "@mui/material"
+import { Box, Button, MenuItem, Select } from '@mui/material';
 
 export default function NetworkControls() {
-  return <Box sx={{ backgroundColor: "darkslategrey", display: "flex", flexDirection: "row" }}>
-    <Box>
+  return (
+    <Box sx={{ backgroundColor: 'darkslategrey', display: 'flex', flexDirection: 'row' }}>
+      {/* <Box>
       <Button>North</Button>
       <Button>East</Button>
       <Button>South</Button>
       <Button>West</Button>
-    </Box>
-    <Box>
-      <Button>Travel to</Button>
-      <Select label="Region" variant="standard">
-        <MenuItem value="sci-fi">Sci-Fi</MenuItem>
-        <MenuItem value="fantasy">Fantasy</MenuItem>
-        <MenuItem value="medieval">Medieval</MenuItem>
-        <MenuItem value="contemporary">Contemporary</MenuItem>
-        <MenuItem value="horror">Horror</MenuItem>
-        <MenuItem value="surreal">Surreal</MenuItem>
-        <MenuItem value="goofy">Goofy</MenuItem>
-      </Select>
+    </Box> */}
+      <Box>
+        <Button>Travel to</Button>
+        <Select label="Region" variant="standard" defaultValue={"none"}>
+          <MenuItem value="none" />
+          <MenuItem value="sci-fi">Sci-Fi</MenuItem>
+          <MenuItem value="fantasy">Fantasy</MenuItem>
+          <MenuItem value="medieval">Medieval</MenuItem>
+          <MenuItem value="contemporary">Contemporary</MenuItem>
+          <MenuItem value="horror">Horror</MenuItem>
+          <MenuItem value="surreal">Surreal</MenuItem>
+          <MenuItem value="goofy">Goofy</MenuItem>
+        </Select>
 
-      <Button>Random Region</Button>
+        <Button>Random Region</Button>
+      </Box>
     </Box>
-  </Box>
+  );
 }
