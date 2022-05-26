1646d262cb4db18d30fe7e52f66115a78f660136
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-26 14:28:30 -0700

    Added getStory thunk, tweaked sample data

--- src/store/graph.js
+++ src/store/graph.js
@@ -18,62 +18,62 @@ const sampleData = [
       information: 'You can hover over words like this to get more information',
       door: 'This door leads to the next room. Hover over that room in the map view to see what might be inside.',
     },
-    neighbors: [2],
+    neighbors: [1],
   },
-  {
-    label: 'The second room',
-    username: 'micha',
-    penName: 'Michanations',
-    id: 2,
-    summary: 'An elaborately decorated room.',
-    description:
-      'This room is festooned with gaudy decorations. Nearly every surface is covered with some kind of intricate pattern.',
-    group: 'TUTORIAL',
-    color: 'blue',
-    tooltips: {
-      decorations: 'Sometimes there will be hidden information in these descriptions',
-      pattern:
-        'One repeated motif is a series of colors arranged in a circle, each color signifying a different region of the landscape.',
-    },
-    neighbors: [1, 3, 4],
-  },
-  {
-    label: 'The third room',
-    username: 'micha',
-    penName: 'Michanations',
-    id: 3,
-    summary: 'A simple room with information placards on one wall.',
-    description:
-      'This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.',
-    group: 'TUTORIAL',
-    color: 'green',
-    tooltips: {
-      information: 'You can hover over words like this to get more information',
-      door: 'This door leads to the next room. Hover over that room in the map view to see what might be inside.',
-    },
-    neighbors: [2],
-  },
-  {
-    label: 'The other third room',
-    username: 'micha',
-    penName: 'Michanations',
-    id: 4,
-    summary: 'A simple room with information placards on one wall.',
-    description:
-      'This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.',
-    group: 'TUTORIAL',
-    color: 'purple',
-    tooltips: {
-      information: 'You can hover over words like this to get more information',
-      door: 'This door leads to the next room. Hover over that room in the map view to see what might be inside.',
-    },
-    neighbors: [2],
-  },
+  // {
+  //   label: 'The second room',
+  //   username: 'micha',
+  //   penName: 'Michanations',
+  //   id: 2,
+  //   summary: 'An elaborately decorated room.',
+  //   description:
+  //     'This room is festooned with gaudy decorations. Nearly every surface is covered with some kind of intricate pattern.',
+  //   group: 'TUTORIAL',
+  //   color: 'blue',
+  //   tooltips: {
+  //     decorations: 'Sometimes there will be hidden information in these descriptions',
+  //     pattern:
+  //       'One repeated motif is a series of colors arranged in a circle, each color signifying a different region of the landscape.',
+  //   },
+  //   neighbors: [1, 3, 4],
+  // },
+  // {
+  //   label: 'The third room',
+  //   username: 'micha',
+  //   penName: 'Michanations',
+  //   id: 3,
+  //   summary: 'A simple room with information placards on one wall.',
+  //   description:
+  //     'This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.',
+  //   group: 'TUTORIAL',
+  //   color: 'green',
+  //   tooltips: {
+  //     information: 'You can hover over words like this to get more information',
+  //     door: 'This door leads to the next room. Hover over that room in the map view to see what might be inside.',
+  //   },
+  //   neighbors: [2],
+  // },
+  // {
+  //   label: 'The other third room',
+  //   username: 'micha',
+  //   penName: 'Michanations',
+  //   id: 4,
+  //   summary: 'A simple room with information placards on one wall.',
+  //   description:
+  //     'This room is completely unadorned, save for a gigantic series of information diagrams on the east wall. There is a single door leading out of the room.',
+  //   group: 'TUTORIAL',
+  //   color: 'purple',
+  //   tooltips: {
+  //     information: 'You can hover over words like this to get more information',
+  //     door: 'This door leads to the next room. Hover over that room in the map view to see what might be inside.',
+  //   },
+  //   neighbors: [2],
+  // },
 ];
 
 const initialState = {
   stories: sampleData,
-  currentStory: sampleData[0],
+  currentStory: null,
 };
 
 const graphSlice = createSlice({
@@ -113,6 +113,17 @@ export const getAPIStories = (nodeId) => async (dispatch) => {
     }
   }
 };
+
+export const getStory = (nodeId) => async (dispatch) => {
+  if (_validateId(nodeId)) {
+    try {
+      const response = await axios(`${API_URL}/story/${nodeId}`)
+      dispatch(setCurrent(response.data));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
 
 function _validateId(nodeId) {
   if (!nodeId || typeof nodeId !== 'number') {
