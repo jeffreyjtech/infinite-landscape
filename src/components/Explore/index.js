3a79f75f002118db7afca062640449ddc9fac5bf
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-27 10:04:13 -0700

    Added useEffect to pull profile data from API

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -1,10 +1,21 @@
 import { Box } from '@mui/material';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import StoryDisplay from './StoryDisplay';
 import NetworkDisplay from './NetworkDisplay';
+import { useEffect } from 'react';
+import { getProfile } from '../../store/profile';
 
 export default function Explore() {
   const currentStory = useSelector((state) => state.graph.currentStory)
+
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (auth.signedIn) {
+      dispatch(getProfile(auth.id));
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box
