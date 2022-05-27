0b888ea1c815a55c78ad8b0d652875f7c67d55c6
Brady Camp <bradyjcamp@gmail.com>
2022-05-27 12:10:16 -0400

    created component for logout button

---
+++ src/components/Auth/LogOut/index.js
@@ -0,0 +1,23 @@
+import Button from "@mui/material/Button";
+import { useDispatch } from "react-redux";
+import { logOutUser } from "../../../store/auth";
+
+
+function LogOut(){
+  let dispatch = useDispatch();
+
+  //use dispatch to call logout reducer
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    dispatch(logOutUser())
+  }
+
+  return (
+    <Button onClick={handleLogOut} color="primary">
+      Log Out
+    </Button>
+  )
+
+}
+
+export default LogOut;
